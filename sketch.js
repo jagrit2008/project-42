92d04bb9ccb179687fcf8d908b6731d8cfe949b6
jagrit2008 <70403360+jagrit2008@users.noreply.github.com>
2020-11-24 23:42:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var hr,mn,sc;
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+
+ 
+  hr=hour();
+  mn=minute();
+  sc=second();
+  angleMode(DEGREES);
+scAngle=map(sc,0,60,0,360)
+}
+
+function draw() {
+  background(255,255,255);  
+
+stroke(255,0,0)
+strokeWeight(7);
+line(0,0,100,0);
+
+
+
+  drawSprites();
+}
